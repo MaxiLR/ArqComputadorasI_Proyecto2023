4d16a88bbfea44d377ef114ed89c4d4975b4c43e
SantyQ02 <santiagonquesada@gmail.com>
2023-06-05 20:25:12 -0300

    Commit del archivo Linux

---
+++ ARM_Project_LINUX.c
@@ -0,0 +1,386 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+// CONSTANTS
+#define PASSWORD "12345"
+#define QUIT_KEY 'q'
+#define DELAY_INTERVAL 250
+#define DEFAULT_DELAY 1000
+
+unsigned int QUIT;
+size_t DELAY = DEFAULT_DELAY;
+
+void Delay(size_t a) {
+  a = a * 100000;
+  while (a--)
+    ;
+}
+
+void MoveCursorToOrigin() {
+  unsigned int i = 10;
+  while (i--)
+    printf("\033[F");
+}
+
+void Clear() {
+  printf("\033[2J");
+  MoveCursorToOrigin();
+
+}
+
+unsigned int Login() {
+  char *right_password = PASSWORD;
+  char input[5];
+  int i = 0;
+  char c;
+  printf("Ingrese su password de 5 digitos: ");
+
+ while (1) {
+    c = getchar();
+
+    if (c == '\n') {
+      input[i] = '\0';
+      break;
+    } else if (c == 127 || c == '\b') {
+      if (i > 0) {
+        i--;
+        printf("\b \b");
+      }
+    } else {
+      if (i < 5) {
+        printf("*");
+        input[i] = c;
+        i++;
+      }
+    }
+    if (i == 5)
+      break;
+  }
+
+input[i] = '\0';
+
+if (strcmp(input, right_password) == 0) {
+  printf("\nAcceso concedido\n");
+  return 1;
+} else {
+  printf("\nAcceso denegado\n");
+  return 0;
+}
+}
+
+void DisplayBinary(unsigned char DISPLAY, unsigned int option) {
+  char display[8];
+  int i = 0;
+  for (unsigned int POINTER = 0x80; POINTER > 0; POINTER = POINTER >> 1) {
+    if (POINTER & DISPLAY) {
+      display[i] = '*';
+      i++;
+    } else {
+      display[i] = '_';
+      i++;
+    }
+  }
+
+  switch (option) {
+  case 1:
+    printf("\033[1;31mSECUENCIA:\033[0m \033[1;36mAuto Fantastico\033[0m\n\n");
+    break;
+  case 2:
+    printf("\033[1;31mSECUENCIA:\033[0m \033[1;36mEl Choque\033[0m\n\n");
+    break;
+  case 3:
+    printf("\033[1;31mSECUENCIA:\033[0m \033[1;36mEl Rebote\033[0m\n\n");
+    break;
+  case 4:
+    printf("\033[1;31mSECUENCIA:\033[0m \033[1;36mEl Espiral\033[0m\n\n");
+    break;
+  case 5:
+    printf("\033[1;31mSECUENCIA:\033[0m \033[1;36mEl Caos\033[0m\n\n");
+    break;
+  case 0:
+    break;
+  }
+
+  printf("%s", display);
+
+  if (option) {
+    printf("\n\n");
+    printf("\033[1;30mPresione la tecla Q para salir\033[0m");
+  }
+
+  MoveCursorToOrigin();
+}
+
+void *keyListener()
+{
+    while (!QUIT)
+    {
+        int key = getchar();
+        if (key == QUIT_KEY)
+            QUIT = 1;
+        else if (key == KEY_UP)
+        {
+            if (DELAY - DELAY_INTERVAL >= 0)
+                DELAY -= DELAY_INTERVAL;
+        }
+        else if (key == KEY_DOWN)
+        {
+            DELAY += DELAY_INTERVAL;
+        }
+    }
+}
+
+void *AutoFantastico() {
+  Clear();
+  while (!QUIT) {
+    unsigned char DISPLAY = 0x80;
+    for (int i = 0; i < 7; i++) {
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 1);
+      DISPLAY = DISPLAY >> 1;
+      Delay(DELAY);
+    }
+    for (int i = 0; i < 7; i++) {
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 1);
+      DISPLAY = DISPLAY << 1;
+      Delay(DELAY);
+    }
+  }
+}
+
+void *ElChoque() {
+  Clear();
+  while (!QUIT) {
+    unsigned char SUB_DISPLAY_1 = 0x80;
+    unsigned char SUB_DISPLAY_2 = 0x01;
+    unsigned char DISPLAY = 0;
+
+    for (int i = 0; i < 7; i++) {
+      DISPLAY = SUB_DISPLAY_1 + SUB_DISPLAY_2;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 2);
+      SUB_DISPLAY_1 = SUB_DISPLAY_1 >> 1;
+      SUB_DISPLAY_2 = SUB_DISPLAY_2 << 1;
+      Delay(DELAY);
+    }
+  }
+}
+
+void *ElRebote() {
+  Clear();
+  while (!QUIT) {
+    unsigned char DISPLAY = 0x80;
+
+    for (int r = 7; r > 0; r--) {
+      for (DISPLAY; DISPLAY != 0b1; DISPLAY = DISPLAY >> 1) {
+        if (QUIT)
+          break;
+        DisplayBinary(DISPLAY, 3);
+        Delay(DELAY);
+      }
+      for (int j = 0; j < r - 1; j++) {
+        if (QUIT)
+          break;
+        DisplayBinary(DISPLAY, 3);
+        DISPLAY = DISPLAY << 1;
+        Delay(DELAY);
+      }
+    }
+    if (QUIT)
+      break;
+    DisplayBinary(DISPLAY, 3);
+    Delay(DELAY);
+  }
+}
+
+void *ElEspiral() {
+  Clear();
+  unsigned char DISPLAY = 0;
+
+  DisplayBinary(DISPLAY, 4);
+
+  while (!QUIT) {
+    unsigned char SUB_DISPLAY_1 = 0x80;
+    unsigned char SUB_DISPLAY_2 = 0x01;
+    DISPLAY = 0;
+
+    for (int i = 0; i < 4; i++) {
+      DISPLAY += SUB_DISPLAY_1;
+      SUB_DISPLAY_1 = SUB_DISPLAY_1 >> 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 4);
+      Delay(DELAY);
+      DISPLAY += SUB_DISPLAY_2;
+      SUB_DISPLAY_2 = SUB_DISPLAY_2 << 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 4);
+      Delay(DELAY);
+    }
+    for (int i = 0; i < 4; i++) {
+      DISPLAY -= SUB_DISPLAY_2;
+      SUB_DISPLAY_2 = SUB_DISPLAY_2 << 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 4);
+      Delay(DELAY);
+      DISPLAY -= SUB_DISPLAY_1;
+      SUB_DISPLAY_1 = SUB_DISPLAY_1 >> 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 4);
+      Delay(DELAY);
+    }
+  }
+}
+
+void *ElCaos() {
+  Clear();
+  unsigned char DISPLAY = 0;
+  while (!QUIT) {
+    unsigned char SUB_DISPLAY_1 = 0x80;
+    unsigned char SUB_DISPLAY_2 = 0x01;
+
+    for (int i = 0; i < 4; i++) {
+      DISPLAY += SUB_DISPLAY_1;
+      SUB_DISPLAY_1 = SUB_DISPLAY_1 >> 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 5);
+      Delay(DELAY);
+      DISPLAY += SUB_DISPLAY_2;
+      SUB_DISPLAY_2 = SUB_DISPLAY_2 << 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 5);
+      Delay(DELAY);
+    }
+    for (int i = 0; i < 4; i++) {
+      DISPLAY -= SUB_DISPLAY_2;
+      SUB_DISPLAY_2 = SUB_DISPLAY_2 >> 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 5);
+      Delay(DELAY);
+      DISPLAY -= SUB_DISPLAY_1;
+      SUB_DISPLAY_1 = SUB_DISPLAY_1 << 1;
+      if (QUIT)
+        break;
+      DisplayBinary(DISPLAY, 5);
+      Delay(DELAY);
+    }
+  }
+}
+
+void App() {
+  unsigned char option[1];
+  Clear();
+
+  int i = 0;
+  for (i; i < 3; i++) {
+    Delay(2000);
+    Clear();
+    if (Login())
+      break;
+  }
+
+  if (i == 3)
+    exit(0);
+
+  do {
+    Delay(2000);
+    DisplayBinary(0, 0);
+    Clear();
+    printf("\033[?25h");
+    QUIT = 0;
+    // DELAY = DEFAULT_DELAY;
+
+    printf("------ S E C U E N C I A S  D E  L U C E S ------\n");
+    printf("1. Auto Fantastico\n");
+    printf("2. El Choque\n");
+    printf("3. El Rebote\n");
+    printf("4. El Espiral\n");
+    printf("5. El Caos\n");
+    printf("0. Salir\n");
+    printf("-------------------------------------------------\n");
+    printf("\nSeleccione una opcion: ");
+
+    scanf("%s1", &option[0]);
+
+    if (option[1] != '\0') {
+      option[0] = 'i';
+    }
+
+    printf("\033[?25l");
+
+    pthread_t threads[2];
+
+    switch (option[0]) {
+
+    case '1':
+      pthread_create(&threads[0], NULL, keyListener, NULL);
+      pthread_create(&threads[1], NULL, AutoFantastico, NULL);
+      pthread_join(threads[0], NULL);
+      pthread_join(threads[1], NULL);
+
+      break;
+    case '2':
+      pthread_create(&threads[0], NULL, keyListener, NULL);
+      pthread_create(&threads[1], NULL, ElChoque, NULL);
+      pthread_join(threads[0], NULL);
+      pthread_join(threads[1], NULL);
+
+      break;
+    case '3':
+      pthread_create(&threads[0], NULL, keyListener, NULL);
+      pthread_create(&threads[1], NULL, ElRebote, NULL);
+      pthread_join(threads[0], NULL);
+      pthread_join(threads[1], NULL);
+
+      break;
+    case '4':
+      pthread_create(&threads[0], NULL, keyListener, NULL);
+      pthread_create(&threads[1], NULL, ElEspiral, NULL);
+      pthread_join(threads[0], NULL);
+      pthread_join(threads[1], NULL);
+
+      break;
+    case '5':
+      pthread_create(&threads[0], NULL, keyListener, NULL);
+      pthread_create(&threads[1], NULL, ElCaos, NULL);
+      pthread_join(threads[0], NULL);
+      pthread_join(threads[1], NULL);
+
+      break;
+    case '0':
+      printf("\nSaliendo del programa...\n");
+      Delay(2000);
+      Clear();
+      exit(0);
+      break;
+    default:
+      printf("\nIngrese una opcion valida\n");
+      break;
+    }
+  } while (1);
+}
+
+int main() {
+  initscr();              // Inicializar la pantalla de ncurses
+  keypad(stdscr, TRUE);   // Habilitar la entrada de teclado especial
+  nodelay(stdscr, TRUE);  // Configurar getch() para no bloquear
+  noecho();               // No mostrar los caracteres ingresados en pantalla
+
+  App();
+
+  endwin();
+}
