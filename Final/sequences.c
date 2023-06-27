ced0c3ed10666d4bc79842dba0f28047e0e84cd0
MaxiLR <maximolr.itr@gmail.com>
2023-06-27 11:49:41 -0300

    Exp Commit

--- Final/sequences.c
+++ Final/sequences.c
@@ -72,7 +72,7 @@ void DisplayBinary(unsigned char DISPLAY, unsigned int option) {
     break;
   }
 
-  printf("%s", display);
+  printf("%s\n\n\r\033[1;33mDELAY: \033[0m%d   ", display, DELAY);
 
   if (option) {
     printf("\n\r\n\r");
